d0231dd01763e9284b0ce6c897e99e7edc474e03
mmnihm <82663561@qq.com>
2026-09-27 01:39:58 +0800

    修复资源目录详情换行排版

--- src/index.js
+++ src/index.js
@@ -1497,7 +1497,7 @@ async function handleDirectoryCallback(token, q, child=false) {
     return tg(token,"editMessageText",{
       chat_id:chatId,
       message_id:messageId,
-      text:"📁 <b>"+safe+"</b>\\n\\n━━━━━━━━━━━━\\n📦 共 <b>"+all.length+"</b> 个资源\\n📤 每次获取 <b>10 个</b>\\n\\n👇 点击下方按钮开始获取\\n━━━━━━━━━━━━",
+      text:"📁 <b>"+safe+"</b>\n\n━━━━━━━━━━━━\n📦 共 <b>"+all.length+"</b> 个资源\n📤 每次获取 <b>10 个</b>\n\n👇 点击下方按钮开始获取\n━━━━━━━━━━━━",
       parse_mode:"HTML",
       reply_markup:folderSummaryKeyboard(d.id,all.length,0)
     });
